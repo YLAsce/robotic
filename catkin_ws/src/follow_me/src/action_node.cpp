1c4f383a07bf9962dafd2036dfbec5483950fe4e
MA Yuqiang <mayuqiang@eduroam-047167.grenet.fr>
2023-03-31 16:30:51 +0200

    a

--- catkin_ws/src/follow_me/src/action_node.cpp
+++ catkin_ws/src/follow_me/src/action_node.cpp
@@ -246,7 +246,7 @@ void combine_rotation_and_translation()
         coef_rotation = 1;
     float coef_translation = 1 - coef_rotation;
 
-    translation_speed = translation_speed_max * coef_translation;//@@IMPD
+    translation_speed = translation_speed * coef_translation;//@@IMPD
     ROS_INFO("coef_rotation: %f, rotation_speed: %f, coef_translation: %f, translation_speed: %f", coef_rotation, rotation_speed*180/M_PI, coef_translation, translation_speed);
 
 }//combine_rotation_and_translation
